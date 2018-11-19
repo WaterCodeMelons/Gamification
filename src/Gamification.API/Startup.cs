7434cc3a57cbc70bfb6f65f4e3d39e4b3c767c63
unknown <sebastian.wolski96@gmail.com>
2018-11-19 22:44:54 +0100

    Fixed brackets

--- src/Gamification.API/Startup.cs
+++ src/Gamification.API/Startup.cs
@@ -54,9 +54,13 @@ namespace Gamification.API
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLifetime)
         {
             if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
+            }
             else
+            {
                 app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
             app.UseMvc();
