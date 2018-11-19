2f4ff9a0092adb36da03ccabd7296e4eade95acc
unknown <sebastian.wolski96@gmail.com>
2018-11-19 22:19:07 +0100

    Added base controller

---
+++ src/Gamification.API/Controllers/ApiBaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gamification.Infrastructure.Commands;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gamification.API.Controllers
+{
+    [ApiController]
+    [EnableCors("FreePolicy")]
+    [Route("api/[controller]")]
+    public abstract class ApiBaseController: Controller
+    {
+        protected ICommandDispatcher CommandDispatcher;
+
+        protected ApiBaseController(ICommandDispatcher commandDispatcher)
+        {
+            CommandDispatcher = commandDispatcher;
+        }
+    }
+}
